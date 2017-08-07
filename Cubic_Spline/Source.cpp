c82d5ccbb15a003e9ec8c1da3b87ebfa9287e9ef
Shu Yang <syang@geodigital.com>
2017-08-07 12:58:03 -0400

    adjust drawing range

--- Cubic_Spline/Source.cpp
+++ Cubic_Spline/Source.cpp
@@ -16,17 +16,17 @@ int main(int argc, char** argv) {
 	
 	double x1 = 0.1; 
 	double y1 = 0.1;
-	for (double x2 = 0; x2 < 2.0; x2 += 0.02) {
+	for (double x2 = 0; x2 < 2.2; x2 += 0.02) {
 		double y2 = spline.getY(x2);
 		std::cout << x2 << ":" << y2 << std::endl;
-		cv::line(myImage, cv::Point(x1 * w / 2, y1 * w / 2), cv::Point(x2 * w / 2, y2 * w / 2), cv::Scalar(0, 255, 0), 2, 0);
+		cv::line(myImage, cv::Point(x1 * w / 3, y1 * w / 3), cv::Point(x2 * w / 3, y2 * w / 3), cv::Scalar(0, 255, 0), 2, 0);
 		std::cout << x1 * w / 2 << ":" << y1 * w / 2 << std::endl;
 		x1 = x2;
 		y1 = y2;
 	}
 
 	for (int k = 0; k < X.size(); k++) {
-		cv::circle(myImage, cv::Point(X[k] * w / 2, Y[k] * w / 2), 4, cv::Scalar(255, 0, 0), 3);
+		cv::circle(myImage, cv::Point(X[k] * w / 3, Y[k] * w / 3), 4, cv::Scalar(255, 0, 0), 3);
 	}
 
 	cv::imshow("image", myImage);
